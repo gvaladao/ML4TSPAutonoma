490d1b89f58fdb6f7c9d4b244f4f34eb82a36c55
ahdediu <adediu@autonoma.pt>
2022-07-13 13:01:42 +0100

    Adrian added knap_sack_dynamic_programming

--- opt_utils.py
+++ opt_utils.py
@@ -129,3 +129,35 @@ class KnapsackInstances:
         self.df['Values'] = items_values
         # return items_weights
         return self.df
+
+
+# A Dynamic Programming based Python
+# Program for 0-1 Knapsack problem
+# Returns the maximum value that can
+# be put in a knapsack of capacity W
+# from https://www.geeksforgeeks.org/python-program-for-dynamic-programming-set-10-0-1-knapsack-problem/
+def knap_sack_dynamic_programming(W, wt, val, n):
+    # W capacity
+    # n items
+    # wt list of weights
+    # val list of values
+    # returns the maximum value
+    # --------we use ----------
+    # a recursive function  K[i,w] to be
+    # the maximum value that can be attained with weight less than or equal to w
+    # using items up to i (first i items).
+
+    K = [[0 for x in range(W + 1)] for x in range(n + 1)]
+
+    # Build table K[][] in bottom up manner
+    for i in range(n + 1):
+        for w in range(W + 1):
+            if i == 0 or w == 0:
+                K[i][w] = 0
+            elif wt[i - 1] <= w:
+                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w])
+            else:
+                K[i][w] = K[i - 1][w]
+
+    return K[n][W]
+    # complexity O(nW)
